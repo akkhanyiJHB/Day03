8b40d756e21d35fdf21db02ea63d72199056ece8
Akhona Khanyile <akkhanyi@c5r11s3.wethinkcode.co.za>
2020-07-17 13:56:08 +0200

    3rd

--- ex01/ft_ultimate_ft.c
+++ ex01/ft_ultimate_ft.c
@@ -6,7 +6,7 @@
 /*   By: akkhanyi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/12 16:07:34 by akkhanyi          #+#    #+#             */
-/*   Updated: 2020/07/13 15:49:13 by akkhanyi         ###   ########.fr       */
+/*   Updated: 2020/07/17 13:55:26 by akkhanyi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,3 @@ void	ft_ultimate_ft(int *********nbr);
 	*********nbr = 42;
 }
 
-int 		main()
-{
-	int a; 
-	ft_ultimate_ft( &a);
-	printf("%d",a);
-}
